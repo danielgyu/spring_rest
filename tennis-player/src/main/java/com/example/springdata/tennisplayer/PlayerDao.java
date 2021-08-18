6da21b905bfee87c55b10cf23c11d5c1d9fe650b
kungyu <hismayfly@gmail.com>
2021-08-19 01:03:38 +0900

    jdbc select

---
+++ tennis-player/src/main/java/com/example/springdata/tennisplayer/PlayerDao.java
@@ -0,0 +1,20 @@
+package com.example.springdata.tennisplayer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PlayerDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Player> getAllPlayers() {
+        String sql = "SELECT * FROM Player";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
+    }
+}
---
+++ tennis-player/src/main/java/com/example/springdata/tennisplayer/TennisPlayerApplication.java
@@ -0,0 +1,24 @@
+package com.example.springdata.tennisplayer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayerApplication {
+
+	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PlayerDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		logger.info("All Players Data: {}", dao.getAllPlayers());
+	}
+}
