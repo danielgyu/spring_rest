c7682a97729ba70854cf3addd99dce32d7a7362e
kungyu <hismayfly@gmail.com>
2021-08-22 23:55:54 +0900

    spring in action, chapter 1

---
+++ taco-cloud/src/test/java/mayfly/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package mayfly.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
