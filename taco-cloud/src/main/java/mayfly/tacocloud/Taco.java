1256ab39dfd0218854d34dd664914d1617a6f422
kungyu <hismayfly@gmail.com>
2021-08-24 23:45:15 +0900

    custom row mapper

---
+++ taco-cloud/src/main/java/mayfly/tacocloud/Taco.java
@@ -0,0 +1,11 @@
+package mayfly.tacocloud;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+    
+    private String name;
+    private List<Ingredient> ingredients;
+}
---
+++ taco-cloud/src/main/java/mayfly/tacocloud/TacoOrder.java
@@ -0,0 +1,24 @@
+package mayfly.tacocloud;
+
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+	this.tacos.add(taco);
+    }
+}
